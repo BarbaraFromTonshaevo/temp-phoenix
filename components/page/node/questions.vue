199e0a4b761760ece6d1845f30f4699468fe3bc4
Elkina Varvara <elkinavarvara1999@gmail.com>
2024-11-28 16:40:48 +0300

    faq page with lint

--- components/page/node/questions.vue
+++ components/page/node/questions.vue
@@ -1,11 +1,83 @@
 <template>
-    <div>
-        <h1 class="title-h1">Question page</h1>
+  <div class="faq">
+    <div class="container">
+      <UiBreadcrumbs :breadcrumbs="data.breadcrumb" />
+      <h1 class="faq__title title-h1">Вопрос-ответ</h1>
+      <ContainerNavigation :navigation-list="nav" class="faq__body">
+        <SectionFaq
+          v-for="navItem of nav"
+          :id="navItem.id"
+          :key="navItem.id"
+          :properties="navItem.properties"
+        />
+      </ContainerNavigation>
     </div>
+  </div>
 </template>
 
 <script setup>
+import { computed } from "vue";
+const props = defineProps({
+  data: {
+    type: Object,
+    default: () => {},
+  },
+});
 
+const nav = computed(() => {
+  const arr = [];
+  for (
+    let sectionIndex = 0;
+    sectionIndex < props.data.data.field_questions.length;
+    sectionIndex++
+  ) {
+    const accordionArray = [];
+    for (
+      let itemIndex = 0;
+      itemIndex <
+      props.data.data.field_questions[sectionIndex].field_questions.length;
+      itemIndex++
+    ) {
+      accordionArray.push({
+        title:
+          props.data.data.field_questions[sectionIndex].field_questions[
+            itemIndex
+          ].field_question[0],
+        body: props.data.data.field_questions[sectionIndex].field_questions[
+          itemIndex
+        ].field_answear[0],
+      });
+    }
+    arr.push({
+      title: props.data.data.field_questions[sectionIndex].field_subject[0],
+      id: "faq-" + props.data.data.field_questions[sectionIndex].id,
+      properties: {
+        title: props.data.data.field_questions[sectionIndex].field_subject[0],
+        data: accordionArray,
+      },
+    });
+  }
+  return arr;
+});
 </script>
-
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.faq {
+  &__title {
+    margin-bottom: 100px;
+  }
+}
+@include desktop {
+  .faq {
+    &__title {
+      margin-bottom: 80px;
+    }
+  }
+}
+@include laptop {
+  .faq {
+    &__title {
+      margin-bottom: 60px;
+    }
+  }
+}
+</style>
---
+++ components/section/faq.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="faq__section">
+    <h3 class="faq__heading title-h3">{{ properties.title }}</h3>
+    <UiAccordion :data="properties.data"/>
+  </section>
+</template>
+
+<script setup>
+defineProps({
+  properties: {
+    type: Object,
+    required: true,
+    default: () => {
+      return { title: "", data: "" };
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+.faq {
+  &__section {
+    width: 100%;
+  }
+  &__heading {
+    margin-bottom: 60px;
+  }
+}
+@include tablet-s {
+  .faq {
+    &__heading {
+      margin-bottom: 30px;
+    }
+  }
+}
+</style>
